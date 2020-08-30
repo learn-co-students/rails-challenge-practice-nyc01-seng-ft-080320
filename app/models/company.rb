4fd427e3ac1b22f023cd09b2599391d18b383de8
https://learn.co/arf2k <foreman.alexander@gmail.com>
2020-08-30 18:30:17 -0400

    more features

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,10 +3,12 @@ class Company < ApplicationRecord
     has_many :offices 
     has_many :buildings, through: :offices 
 
-def location 
-    self.offices.map do |office|
-        "#{office.building.name}, floor #{office.floor}"
+
+
+
+    def rent
+        self.buildings.map{|building| building.rent_per_floor}.sum 
     end
-end
+
 
 end
--- app/models/office.rb
+++ app/models/office.rb
@@ -2,4 +2,8 @@ class Office < ApplicationRecord
     belongs_to :building
     belongs_to :company
 
+
+    def location
+        "#{self.building.name}, floor #{self.floor}"
+    end
 end
